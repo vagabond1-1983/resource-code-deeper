a3ae40c364b138cd3174845e52b0980c2817288e
vagabond1_1983 <vagabond1_1983@163.com>
2019-04-26 21:47:56 +0800

    leetcode 27

--- src/main/java/com/vaga/java/leetcode/arraay/RemoveElement.java
+++ src/main/java/com/vaga/java/leetcode/arraay/RemoveElement.java
@@ -22,13 +22,13 @@ public class RemoveElement {
     @DataProvider(name = "data")
     public Object[][] getData() {
         return new Object[][]{
-                {new int[]{3, 2, 2, 3}, 3, 2},
-                {new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5}
+                {new int[]{3, 2, 2, 3}, 3, 2, new int[]{2, 2}},
+                {new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2, 5, new int[]{0, 1, 3, 0, 4}}
         };
     }
 
     @Test(dataProvider = "data")
-    public void mySolution(int[] array, int removedElement, int expectedLength) {
+    public void mySolution(int[] array, int removedElement, int expectedLength, int[] expectedArray) {
         int actualLength = array.length;
         for (int i : array) {
             if (i == removedElement) {
@@ -36,5 +36,23 @@ public class RemoveElement {
             }
         }
         assertThat(actualLength).isEqualTo(expectedLength);
+    }
+
+    @Test(dataProvider = "data")
+    public void theirSolution(int[] array, int removedElement, int expectedLength, int[] expectedArray) {
+        int i = 0;
+        int n = array.length;
+        while (i < n) {
+            if (array[i] == removedElement) {
+                array[i] = array[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        int[] actualArray = new int[n];
+        System.arraycopy(array, 0, actualArray, 0, n);
+        assertThat(actualArray).hasSize(expectedLength).containsExactlyInAnyOrder(expectedArray);
+
     }
 }
