c16abee1c60ce480647d4b7a9878196f0e68865f
vagabond1_1983 <vagabond1_1983@163.com>
2019-05-17 22:56:54 +0800

    selenide demo

---
+++ src/main/java/com/vaga/selenide/testGoogle.java
@@ -0,0 +1,23 @@
+package com.vaga.selenide;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class testGoogle {
+    @Test
+    public void openGoogle() throws InterruptedException {
+        Configuration.browser = "Chrome";
+        Configuration.baseUrl = "https://www.baidu.com";
+        open("/");
+        $("#kw").setValue("selenide");
+        $("#su").submit();
+        Thread.sleep(3000);
+        // 断言
+        $$("h3 > a").shouldHave(size(10));
+        $("h3 > a").setValue(String.valueOf(text("selenide_百度翻译")));
+    }
+}
